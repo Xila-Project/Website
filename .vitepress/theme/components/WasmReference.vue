b554a853f78837c956ba8c5fb045852e79e1cd4f
Alix ANNERAUD <alix@anneraud.fr>
2026-01-12 22:47:15 +0100

    Add HostReference and WasmReference components with auto-registration in theme

---
+++ .vitepress/theme/components/WasmReference.vue
@@ -0,0 +1,36 @@
+<template>
+  <a :href="href" class="wasm-reference-link">
+    <slot>{{ text }}</slot>
+  </a>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  path: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    default: "",
+  },
+});
+
+const href = computed(() => {
+  return `/wasm-reference?path=${encodeURIComponent(props.path)}`;
+});
+</script>
+
+<style scoped>
+.wasm-reference-link {
+  color: var(--vp-c-brand-1);
+  text-decoration: none;
+}
+
+.wasm-reference-link:hover {
+  color: var(--vp-c-brand-2);
+  text-decoration: underline;
+}
+</style>
