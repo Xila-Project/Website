e7ff7c4ae48f86d367e0e4592a4b70e10bbe2c70
Alix ANNERAUD <alix@anneraud.fr>
2026-01-14 21:35:01 +0100

    feat: refactor component registration logic and add Vue as a dependency

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,24 +1,29 @@
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import { App } from "vue";
+
+function registerComponents(app: App<any>) {
+  // Auto-register all components in the components directory
+  const components = import.meta.glob("./components/**/*.vue", {
+    eager: true,
+  });
+
+  for (const path in components) {
+    const component = components[path] as any;
+    const componentName = path
+      .split("/")
+      .pop()
+      ?.replace(/\.\w+$/, ""); // Remove file extension
+
+    if (componentName) {
+      app.component(componentName, component.default);
+    }
+  }
+}
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    // Auto-register all components in the components directory
-    const components = import.meta.glob("./components/**/*.vue", {
-      eager: true,
-    });
-
-    for (const path in components) {
-      const component = components[path] as any;
-      const componentName = path
-        .split("/")
-        .pop()
-        ?.replace(/\.\w+$/, ""); // Remove file extension
-
-      if (componentName) {
-        app.component(componentName, component.default);
-      }
-    }
+    registerComponents(app);
   },
 } satisfies Theme;
