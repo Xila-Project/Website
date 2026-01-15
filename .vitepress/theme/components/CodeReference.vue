8dfef5ab72d566722923c32de790e90c3f8979ad
Alix ANNERAUD <alix@anneraud.fr>
2026-01-15 21:11:41 +0100

    feat: add CodeReference and DocsRsReference components for external links

---
+++ .vitepress/theme/components/CodeReference.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { computed } from "vue";
+import { getReferenceDisplayName } from "../utilities/reference";
+
+const props = defineProps({
+  path: {
+    type: String,
+    required: true,
+  },
+});
+
+const href = computed(() => {
+  return `https://github.com/Xila-Project/Core/tree/main/${props.path}`;
+});
+const label = computed(() => `<code>${props.path}</code>`);
+</script>
+
+<template>
+  <a :href="href" class="external-reference-link">
+    <slot><span v-html="label"></span></slot>
+  </a>
+</template>
+
+<style scoped>
+.external-reference-link {
+  color: var(--vp-c-brand-1);
+  text-decoration: none;
+}
+
+.external-reference-link:hover {
+  color: var(--vp-c-brand-2);
+  text-decoration: underline;
+}
+</style>
