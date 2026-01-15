4f66ba9209cb6f5bd7874489eae6f1e1cff29297
Alix ANNERAUD <alix@anneraud.fr>
2026-01-15 13:20:28 +0100

    feat: add ExternalReference component for dynamic external links

---
+++ .vitepress/theme/components/ExternalReference.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { computed } from "vue";
+import { getReferenceDisplayName } from "../utilities/reference";
+
+const props = defineProps({
+  crate: {
+    type: String,
+    required: true,
+  },
+});
+
+const href = computed(() => {
+  return `https://docs.rs/${props.crate}`;
+});
+const label = computed(() => getReferenceDisplayName(props.crate));
+</script>
+
+<template>
+  <a :href="href" class="external-reference-link">
+    <slot><span v-html="label"></span></slot>
+  </a>
+</template>
+
+<style scoped>
+.external-reference-link {
+  color: var(--vp-c-brand-1);
+  text-decoration: none;
+}
+
+.external-reference-link:hover {
+  color: var(--vp-c-brand-2);
+  text-decoration: underline;
+}
+</style>
